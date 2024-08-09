1c2b5148d47dbc1d5edf00186d6adf656b1116f6
uzay <uzayilica2@gmail.com>
2024-08-09 14:28:12 +0300

    register işlemleri eklendi

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -12,8 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.function.Consumer;
+
 @Configuration
-@EnableWebSecurity(debug = true)
+
 public class SecurityConfig {
 
     //dependecny injection
@@ -41,13 +44,14 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return
                 http
-                        .authorizeHttpRequests(req->req
-                                .requestMatchers(HttpMethod.GET).permitAll()
-                                .requestMatchers("/login","/register").permitAll()
-                                .anyRequest().authenticated())
 
                         .csrf(AbstractHttpConfigurer::disable)
                         .cors(AbstractHttpConfigurer::disable)
+                        .authorizeHttpRequests(req->req
+                                .requestMatchers(HttpMethod.GET).permitAll()
+                                .requestMatchers("/api/auth/v1/**").permitAll()
+                        )
+                        .httpBasic(Customizer.withDefaults())
                         .build();
     }
 
